9d30d207f23287f515e0bfa1293f465b72af53db
gongchao <zhangqiaocheng@fulan.com.cn>
2021-01-15 16:35:11 +0800

    gateway

---
+++ nacos-gateway/src/main/java/com/example/nacosgateway/NacosGatewayApplication.java
@@ -0,0 +1,20 @@
+package com.example.nacosgateway;
+
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@NacosPropertySource(dataId = "nacos-gateway-config", autoRefreshed = true)
+@RefreshScope
+public class NacosGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosGatewayApplication.class, args);
+    }
+
+}
