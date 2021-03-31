73016220d3c8381f46a3fd8522bf7c91ab72d8d6
gongchao <gongchaoh@aliyun.com>
2021-03-31 17:11:27 +0800

    测试:消费方-fegin-提供方

--- nacos-discovery-consumer/src/main/java/springnacos/nacosdiscoveryconsumer/NacosDiscoveryConsumerApplication.java
+++ nacos-discovery-consumer/src/main/java/springnacos/nacosdiscoveryconsumer/NacosDiscoveryConsumerApplication.java
@@ -3,20 +3,22 @@ package springnacos.nacosdiscoveryconsumer;
 import com.alibaba.nacos.api.annotation.NacosProperties;
 import com.alibaba.nacos.api.config.ConfigType;
 import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import springnacos.nacosdiscoveryconsumer.fegin.ProviderFegin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
 @NacosPropertySource(dataId = "nacos-discovery-consumer.yml", autoRefreshed = true, type = ConfigType.YAML, properties = @NacosProperties(namespace = "b258991f-3fb4-43b8-9dd1-0c6c04620f03"))
 @RefreshScope
@@ -29,16 +31,20 @@ public class NacosDiscoveryConsumerApplication {
 		@Autowired
 		private RestTemplate restTemplate;
 
+		@Autowired
+		private ProviderFegin providerFegin;
+
 		@Value("nacos-discovery-consumer")
 		private String appName;
 
 		@GetMapping("/echo/app-name")
 		public String echoAppName(){
 			//使用 LoadBalanceClient 和 RestTemolate 结合的方式来访问
-			ServiceInstance serviceInstance = loadBalancerClient.choose("nacos-discovery-provider");
-			String url = String.format("http://%s:%s/echo/%s",serviceInstance.getHost(),serviceInstance.getPort(),appName);
-			System.out.println("request url:"+url);
-			return restTemplate.getForObject(url,String.class);
+//			ServiceInstance serviceInstance = loadBalancerClient.choose("nacos-discovery-provider");
+//			String url = String.format("http://%s:%s/echo/%s",serviceInstance.getHost(),serviceInstance.getPort(),appName);
+//			System.out.println("request url:"+url);
+//			return restTemplate.getForObject(url,String.class);
+			return providerFegin.removeAlyVideo("appName");
 		}
 
 	}
---
+++ nacos-discovery-consumer/src/main/java/springnacos/nacosdiscoveryconsumer/fegin/ProviderFegin.java
@@ -0,0 +1,15 @@
+package springnacos.nacosdiscoveryconsumer.fegin;
+
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient("nacos-discovery-provider")
+@Component
+public interface ProviderFegin {
+
+    @GetMapping("/echo/{string}")
+    public String removeAlyVideo(@PathVariable String string);
+}
